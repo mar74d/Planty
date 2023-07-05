5857dbf525486bfe2894b068842e8e7ea4941537
mar74d <marionmeridoux@hotmail.fr>
2023-07-05 11:20:53 +0200

    page Nous rencontrer

---
+++ wp-content/themes/astra-child/header.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The header for Astra Theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?><!DOCTYPE html>
+<?php astra_html_before(); ?>
+<html <?php language_attributes(); ?>>
+<head>
+<?php astra_head_top(); ?>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://localhost/Planty/wp-content/themes/astra-child/style.css"> 
+<?php 
+if ( apply_filters( 'astra_header_profile_gmpg_link', true ) ) {
+	?>
+	 <link rel="profile" href="https://gmpg.org/xfn/11"> 
+	 <?php
+} 
+?>
+<?php wp_head(); ?>
+<?php astra_head_bottom(); ?>
+</head>
+
+<body
+      >
+<header>
+                <div id="logo_image">
+                <img src="<?= get_stylesheet_directory_uri()."/images/Logo.png" ?>" alt="Logo du site Planty" title="Logo du site Planty">
+                <!-- génère un lien absolu vers dossier images !-->
+                </div>
+                    
+                <?php wp_nav_menu() ?>
+
+
+            </header>
